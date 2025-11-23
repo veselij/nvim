8a5ebf785d4084729bb92baf5c5706e3ecb23577
veselij <veselij85@gmail.com>
2025-11-23 15:26:44 +0100

    pyrigh code style change

--- lsp/pyright.lua
+++ lsp/pyright.lua
@@ -1,24 +1,24 @@
 return {
-        cmd = { 'pyright-langserver', '--stdio' },
-        filetypes = { 'python', 'py' },
-        root_markers = {
-                "pyproject.toml",
-                "setup.py",
-                "setup.cfg",
-                "requirements.txt",
-                "pyrightconfig.json",
-        },
-        settings = {
-                python = {
-                        analysis = {
-                                autoSearchPaths = true,
-                                useLibraryCodeForTypes = true,
-                                autoImportCompletions = true,
+    cmd = { 'pyright-langserver', '--stdio' },
+    filetypes = { 'python', 'py' },
+    root_markers = {
+        "pyproject.toml",
+        "setup.py",
+        "setup.cfg",
+        "requirements.txt",
+        "pyrightconfig.json",
+    },
+    settings = {
+        python = {
+            analysis = {
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+                autoImportCompletions = true,
 
-                        },
-                        pythonPath = "./.venv/bin/python3",
-                        disableOrganizeImports = true,
-                },
-
+            },
+            pythonPath = "./.venv/bin/python3",
+            disableOrganizeImports = true,
         },
+
+    },
 }
